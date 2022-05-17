1efd85368cdfc97840142872c9740cd1e7fdc744
antoinecaputo <antoine.caputo1@viacesi.fr>
2022-05-17 09:20:49 +0200

    add csv constants

--- constants/constants.go
+++ constants/constants.go
@@ -7,3 +7,12 @@ import (
 var Log *golog.TypLog
 
 const PORT = 8080
+
+const (
+	CSV_HEADER = "user,date,color,value,evaluation"
+	COLOR_1    = "primary"
+	COLOR_2    = "text_primary"
+	COLOR_3    = "text_secondary"
+	COLOR_4    = "button_primary"
+	COLOR_5    = "button_secondary"
+)
