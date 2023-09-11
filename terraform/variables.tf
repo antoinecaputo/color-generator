21e52a42aeaf4f3fef3174f9b1e97e4917e1ebe1
antoine <antoine.caputo1@viacesi.fr>
2023-09-11 18:29:36 +0200

    Terraform docker build and deployment

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "container_name" {
+  description = "Value of the name for the Docker container"
+  type        = string
+  default     = "ColorGeneratorContainer"
+}
+
+variable "volume_name" {
+  description = "Value of the name for the Docker volume"
+  type        = string
+  default     = "ColorGeneratorVolume"
+}
